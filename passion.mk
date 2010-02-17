f56ae2041ceb9989123f7bad96496c956378196a
Jean-Baptiste Queru <jbq@google.com>
2010-02-17 11:14:33 -0800

    Move open-sourceable passion files
    Change-Id: Ie7ff79cea039f5c703c319de94b887b60a9f2a15

---
+++ passion.mk
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# media profiles and capabilities spec
+$(call inherit-product, device/htc/passion-common/media_profiles.mk)
+$(call inherit-product, device/htc/passion-common/media_a1026.mk)
+
+# stuff common to all HTC phones
+$(call inherit-product, device/htc/common/common.mk)
+
+# WARNING: the most specific overlay goes first
+DEVICE_PACKAGE_OVERLAYS := device/htc/passion-common/overlay $(DEVICE_PACKAGE_OVERLAYS)
+
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
+
+# media config xml file
+PRODUCT_COPY_FILES += \
+    device/htc/passion-common/media_profiles.xml:system/etc/media_profiles.xml
+
+PRODUCT_PACKAGES += \
+    sensors.mahimahi \
+    lights.mahimahi
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Passion uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
+PRODUCT_COPY_FILES += \
+    device/htc/passion-common/mahimahi-keypad.kl:system/usr/keylayout/mahimahi-keypad.kl \
+    device/htc/passion-common/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/passion-common/vold.fstab:system/etc/vold.fstab
