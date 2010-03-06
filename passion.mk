d52676ecbb37f654711ad2c1b307e7b12fae44c1
Dan Morrill <morrildl@google.com>
2010-03-05 16:19:14 -0800

    Updating the features definition for Passion to handle the new optional-feature system.

--- passion.mk
+++ passion.mk
@@ -27,8 +27,11 @@ PRODUCT_PROPERTY_OVERRIDES +=
 DEVICE_PACKAGE_OVERLAYS := device/htc/passion-common/overlay $(DEVICE_PACKAGE_OVERLAYS)
 
 PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
