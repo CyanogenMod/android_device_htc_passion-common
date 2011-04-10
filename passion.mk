1cf248905c8fc61828087b10e1df4fefb7ee339e
Steve Kondik <shade@chemlab.org>
2011-04-10 19:34:20 -0400

    Revert "passion: Build libOmxVdec from source"
    This reverts commit ab08f265339c9188a4435da73b980ae0afada6fc.
    
    Will revisit after 7.0 release.

--- passion.mk
+++ passion.mk
@@ -42,8 +42,7 @@ PRODUCT_PACKAGES := \
     copybit.qsd8k \
     gps.mahimahi \
     libOmxCore \
-    libOmxVidEnc \
-    libOmxVdec
+    libOmxVidEnc
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
